5587db7b9071a0a9dd588c5cc1c7462e3750f853
Abdujabborov Biloliddin <tursinalinurmatov8@gmail.com>
2026-08-23 12:18:44 +0500

    Remove SMS cancellation request from test.py

--- test.py
+++ test.py
@@ -1,9 +1,1 @@
-import requests
 
-URL = 'https://seensms.uz/api/v1'
-
-r = requests.post(URL, data={
-    'key':'TOwvqejSqTUCzmIrN8f6jCFC10Z5wIpR', 'action':'cancel',
-    'order':734580
-})
-print(r.json())
