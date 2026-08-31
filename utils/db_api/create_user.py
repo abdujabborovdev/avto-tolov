245fe2e0dd608a7ed7b9bfee10bb6d3b5fad2dd6
Abdujabborov Biloliddin <tursinalinurmatov8@gmail.com>
2026-08-31 16:09:40 +0500

    Update create_user.py

--- utils/db_api/create_user.py
+++ utils/db_api/create_user.py
@@ -56,3 +56,8 @@ async_session = async_sessionmaker(
 async def init_db():
     async with engine.begin() as conn:
         await conn.run_sync(Base.metadata.create_all)
+
+
+async def get_db():
+    async with async_session() as session:
+        yield session
