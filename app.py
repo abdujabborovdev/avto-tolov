a29eea2fbf9fa7011565d7cb464a32cdfebfb122
Biloliddin <bilolidding@gmail.com>
2026-08-31 12:36:24 +0500

    salom

--- app.py
+++ app.py
@@ -1,6 +1,6 @@
 import asyncio
 import logging
-
+from utils.db_api.create_user import init_db # init_db ni import qilasiz
 from loader import bot, dp
 import middlewares, filters, handlers
 from utils.notify_admins import on_startup_notify
@@ -13,6 +13,7 @@ async def on_startup():
 
 
 async def main():
+    await init_db()
     logging.basicConfig(level=logging.INFO)
     middlewares.setup(dp)
     filters.setup(dp)
